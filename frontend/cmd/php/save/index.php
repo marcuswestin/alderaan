bfd9b0af5e86b1b441fe9f0d696b93c26e8b77bc
Marcus Westin <narcvs@gmail.com>
2009-09-25 11:36:02 -1000

    Add the save command for the PHP backend.
    Move some functionality from open to settings.php.inc to share code between open and save

---
+++ frontend/cmd/php/save/index.php
@@ -0,0 +1,8 @@
+<?php
+	include '../settings.php.inc';
+	
+	$file = bespin_get_file_name($_GET['path']);
+	$fh = fopen($file, 'w') or bespin_error("Could not open file " . $file . " for writing");
+	fwrite($fh, file_get_contents('php://input'));
+	fclose($fh);
+?>
