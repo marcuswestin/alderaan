4816e5f9bb80b0ab547318af057b5da2d364ab99
Marcus Westin <narcvs@gmail.com>
2009-09-27 17:27:05 -1000

    Call resizeTree once after the document has initialized to ensure the file tree is sized properly.

--- frontend/js/bespin/tree/init.js
+++ frontend/js/bespin/tree/init.js
@@ -1,13 +1,18 @@
 jQuery(document).ready( function($) {
 	
-	$('#fileTree').fileTree({ root: '.', script: 'cmd/php/ls/' }, function(path) { 
+	function resizeTree() {
+	    $('#fileTree').height($(window).height());
+	    // window.top.console.debug('resize', $(window).width(), $(window).height());
+	}
+    
+    function onFileClick(path) {
         bespin.get("editor").openFile(bespin.get('editSession').project, path, {});
         // bespin.publish("ui:escape", {});
-	});
+    }
+
+	$('#fileTree').fileTree({ root: '.', script: 'cmd/php/ls/' }, onFileClick);
+	$(window).bind('resize', resizeTree);
 	
-	$(window).bind('resize', function(){
-        $('#fileTree').height($(window).height());
-        // window.top.console.debug('resize', $(window).width(), $(window).height());
-	})
+	resizeTree();
 });
 
