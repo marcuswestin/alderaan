27fdbc9f1a39854bb721d99998b8519dffce973c
Marcus Westin <marcus@dev2.dev.meebo.com>
2009-10-01 12:15:01 -0700

    Make all exec commands work in the default directory

--- cmd/php/executeCommand/index.php
+++ cmd/php/executeCommand/index.php
@@ -1,4 +1,8 @@
 <?php
+    include '../util.php.inc';
     // nice and simple :)
-    passthru(urldecode($_GET['commandString']) . ' 2>&1');
+    global $bespin_bases;
+    $dir = $bespin_bases['Default'];
+
+    passthru("cd $dir; " . urldecode($_GET['commandString']) . ' 2>&1');
 ?>
