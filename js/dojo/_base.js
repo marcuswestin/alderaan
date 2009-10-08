572b8a47c03c10a80833e3503a903191bdf729c6
Marcus Westin <narcvs@gmail.com>
2009-10-08 00:58:15 -0700

    Whitespace fix

--- js/dojo/_base.js
+++ js/dojo/_base.js
@@ -5,15 +5,15 @@
 */
 
 
-if(!dojo._hasResource["dojo._base"]){
-dojo._hasResource["dojo._base"]=true;
-dojo.provide("dojo._base");
-dojo.require("dojo._base.lang");
-dojo.require("dojo._base.declare");
-dojo.require("dojo._base.connect");
-dojo.require("dojo._base.Deferred");
-dojo.require("dojo._base.json");
-dojo.require("dojo._base.array");
-dojo.require("dojo._base.Color");
-dojo.requireIf(dojo.isBrowser,"dojo._base.browser");
+if(!dojo._hasResource["dojo._base"]) {
+	dojo._hasResource["dojo._base"] = true;
+	dojo.provide("dojo._base");
+	dojo.require("dojo._base.lang");
+	dojo.require("dojo._base.declare");
+	dojo.require("dojo._base.connect");
+	dojo.require("dojo._base.Deferred");
+	dojo.require("dojo._base.json");
+	dojo.require("dojo._base.array");
+	dojo.require("dojo._base.Color");
+	dojo.requireIf(dojo.isBrowser,"dojo._base.browser");
 }
