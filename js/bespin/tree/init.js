611eb17e6d61dd98c3864652e4543c5c76d697aa
Marcus Westin <narcvs@gmail.com>
2009-10-11 14:46:43 -0700

    Remove commented lines of code

--- js/bespin/tree/init.js
+++ js/bespin/tree/init.js
@@ -2,12 +2,10 @@ jQuery(document).ready( function($) {
 	
 	function resizeTree() {
 	    $('#fileTree').height($(window).height());
-	    // window.top.console.debug('resize', $(window).width(), $(window).height());
 	}
     
     function onFileClick(path) {
         bespin.get("editor").openFile(bespin.get('editSession').project, path, {});
-        // bespin.publish("ui:escape", {});
     }
 
 	$('#fileTree').fileTree({ root: '.', script: 'cmd/php/ls/' }, onFileClick);
